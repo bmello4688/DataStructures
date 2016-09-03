87709c6b82473fac6e636da9e368b558968a7f18
bmello4688 <bmello4688@live.com>
2016-09-03 11:45:24 -0400

    create more overload methods

--- Source/Workspaces/Band/ConsoleApplication1/Graph/GraphSearch.cs
+++ Source/Workspaces/Band/ConsoleApplication1/Graph/GraphSearch.cs
@@ -23,10 +23,20 @@ namespace DataStructures
 
         public abstract void ExecuteSearch(Vertex startingVertex);
 
-        public List<Vertex> GetShortestPath(Vertex target)
+        public List<Vertex> GetShortestPath(int startVertex = 0, int targetVertex)
+        {
+            return GetShortestPath(Graph[startVertex], Graph[targetVertex]);
+        }
+
+        public List<Vertex> GetShortestPath(Vertex targetVertex)
+        {
+            return GetShortestPath(Graph[0], targetVertex);
+        }
+
+        public List<Vertex> GetShortestPath(Vertex startVertex, Vertex targetVertex)
         {
             List<Vertex> path = new List<Vertex>();
-            Vertex step = target;
+            Vertex step = targetVertex;
             // check if a path exists
             if (!PathPredecessors.ContainsKey(step))
                 return null;
@@ -37,12 +47,30 @@ namespace DataStructures
             {
                 step = PathPredecessors[step];
                 path.Add(step);
+
+                if (step == startVertex)
+                    break;//Found starting vertex so end path
             }
 
             // Reverse because current list is from end to start
             path.Reverse();
 
             return path;
+        }
+
+        public int GetShortestPathCost(int target)
+        {
+            return GetShortestPathCost(Graph[target]);
+        }
+
+        public int GetShortestPathCost(Vertex target)
+        {
+            List<Vertex> shortestPath = GetShortestPath(target);
+
+            int cost = 0;
+            shortestPath.ForEach(vertex => cost += vertex.Weight);
+
+            return cost;
         }
 
         protected int GetDistance(Vertex startingVertex, Vertex endingVertex)
