ea3e685483b2e7d146c813332e6f84abf8c94bd5
bmello4688 <bmello4688@live.com>
2016-10-13 17:24:14 -0400

    update find paths

--- Source/Workspaces/Band/ConsoleApplication1/Graph/FindAllPathsSearch.cs
+++ Source/Workspaces/Band/ConsoleApplication1/Graph/FindAllPathsSearch.cs
@@ -7,8 +7,9 @@ namespace DataStructures
 {
     public class FindAllPathsSearch<TVertex> : GraphSearch<TVertex> where TVertex : Vertex
     {
-        Dictionary<Edge, bool> visitedDictionary = new Dictionary<Edge, bool>();
-        List<Path<TVertex>> paths = new List<Path<TVertex>>();
+        private Dictionary<Edge, bool> visitedDictionary = new Dictionary<Edge, bool>();
+        private Dictionary<Vertex, bool> visitedVertexDictionary = new Dictionary<Vertex, bool>();
+        private List<Path<TVertex>> paths = new List<Path<TVertex>>();
 
         public FindAllPathsSearch(Graph<TVertex> graph)
             :base(graph)
@@ -19,6 +20,8 @@ namespace DataStructures
         {
             Stack<TVertex> vertexStack = new Stack<TVertex>();
 
+            TVertex lastVertex = Graph[Graph.LastVertexNumber];
+            visitedVertexDictionary[startingVertex] = true;
             vertexStack.Push(startingVertex);
 
             while (vertexStack.Count > 0)
@@ -27,12 +30,13 @@ namespace DataStructures
 
                 bool foundUnvisitedEdge = false;
                 //ignore backward paths
-                foreach (var edge in vertex.Edges.Where(edge => edge.EndingVertex.Number > edge.StartingVertex.Number))
+                foreach (var edge in vertex.Edges)
                 {
-                    if (!visitedDictionary[edge])
+                    if (!visitedDictionary[edge] && !visitedVertexDictionary[edge.EndingVertex])
                     {
                         foundUnvisitedEdge = true;
                         visitedDictionary[edge] = foundUnvisitedEdge;
+                        visitedVertexDictionary[vertex] = foundUnvisitedEdge;
                         PathPredecessors[edge.EndingVertex] = vertex;
                         vertexStack.Push((TVertex)edge.EndingVertex);
                         break;
@@ -41,7 +45,7 @@ namespace DataStructures
 
                 if (!foundUnvisitedEdge)
                     vertexStack.Pop();
-                else if(vertexStack.Peek() == Graph[Graph.LastVertexNumber])
+                else if(vertexStack.Peek() == lastVertex)
                 {
                     //found path
                     paths.Add(GetShortestPath(startingVertex, vertexStack.Peek()));
@@ -58,6 +62,19 @@ namespace DataStructures
                     visitedDictionary.Add(edge, false);
                 else
                     visitedDictionary[edge] = false;
+            }
+
+            ClearVerticesVisited();
+        }
+
+        private void ClearVerticesVisited()
+        {
+            foreach (var vertex in Graph.GetVertices())
+            {
+                if (!visitedVertexDictionary.ContainsKey(vertex))
+                    visitedVertexDictionary.Add(vertex, false);
+                else
+                    visitedVertexDictionary[vertex] = false;
             }
         }
 
